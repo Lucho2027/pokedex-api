b2255ba0e8d090d5bc769f3f2ed56fc868528d3f
Lucho2027 <luis01.ro@gmail.com>
2019-12-15 15:30:59 -0600

    "2nd Commit Authorization"

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-
+require('dotenv').config()
 const express=require('express');
 const morgan = require('morgan');
 
 const app= express();
 
-app.use(morgan('dev'));
 
-app.use((req,res)=>{
-    res.send('Hello, world!')
+app.use(morgan('common'));
+
+
+
+app.use(function validateBearerToken(req,res,next){
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+console.log('validate bearer token')
+if(!authToken || authToken.split(' ')[1] !== apiToken){
+    return res.status(401).json({error:'Unauthorized request'})
+}
+next()
 })
+const validTypes=[`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req,res){
+res.json(validTypes)
+}
+app.get('/types',handleGetTypes)
+
+function handleGetPokemon(req,res){
+    res.send('Hello, Pokemon!')
+}
+app.get('/pokemon', handleGetPokemon)
 
 const PORT = 8000;
 
